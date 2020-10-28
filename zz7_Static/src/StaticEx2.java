04dc25c78dbea5d4aa85d1376f65e5f9aafffd43
baker <baker@211.230.78.95>
2020-10-28 20:31:53 +0900

    # WARNING: head commit changed in the meantime
    ffff

---
+++ zz7_Static/src/StaticEx2.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class StaticEx2 {
+	static int num; //무조건 0으로 초기화
+	 
+//	static {
+//		Random rand = new Random();
+//
+//		num = rand.nextInt(100);
+//	}
+	public static void main(String[] args) {
+		System.out.println(num);
+		
+	}
+
+}
